91521e18a37d5beb3c27675ebc295ca5e7efb4c7
kacharuk <kacharuk@gmail.com>
2022-05-26 16:33:47 +0700

    first commit

---
+++ src/main/PricingUtils.java
@@ -0,0 +1,24 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PricingUtils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Product p1 = new Product();
+		Product p2 = new Product();
+		Product p3 = new InHouseProduct();
+		
+		List<Product>  productList = new ArrayList();
+		productList.add(p1);
+		productList.add(p2);
+		productList.add(p3);
+		
+		for (Product product : productList) {
+			System.out.println(product.getDiscount());
+		}
+	}
+
+}
